04f144b0a94b155c58ac09abbdc742111ac25f45
Arek <arkinio91@gmail.com>
2021-09-18 12:31:53 +0200

    add main.js and add pop

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.article__photo').addEventListener("click", function () {
+ document.querySelector('.modal-wrap').classList.add('active');
+ 
+ // document.querySelector('.article').classList.add('blur')
+})
+
+
+document.querySelector('span.hide').addEventListener("click", function () {
+ document.querySelector('.modal-wrap').classList.remove('active');
+ // document.querySelector('.article').classList.remove('blur')
+})
